7f6b34a8e2a2e2b36a5e41b17c76bbf22c1faff2
tolentinoel <rckrleyn@gmail.com>
2020-10-31 15:37:13 -0700

    commit tree 8468c10f445b98bb451986bf07ecddfa2a8199a5

--- db/migrate/20160112163253_create_songs.rb
+++ db/migrate/20160112163253_create_songs.rb
@@ -2,8 +2,8 @@ class CreateSongs < ActiveRecord::Migration
   def change
     create_table :songs do |t|
       t.string :title
+      t.integer :note_id
 
-      t.timestamps null: false
     end
   end
 end
